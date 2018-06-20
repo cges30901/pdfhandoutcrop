e885bfef905b802c48b26d9c4e01febd29556970
Hsiu-Ming Chang <cges30901@gmail.com>
2018-06-20 22:00:06 +0800

    change user interface, using combobox to support more pages per sheet

--- pdfhandoutcrop/mainwindow.py
+++ pdfhandoutcrop/mainwindow.py
@@ -14,12 +14,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.setWindowIcon(QIcon(os.path.dirname(os.path.abspath(__file__))+'/pdfhandoutcrop.png'))
-        self.spbPage=[[self.spbPage1X, self.spbPage1Y],
-            [self.spbPage2X, self.spbPage2Y],
-            [self.spbPage3X, self.spbPage3Y],
-            [self.spbPage4X, self.spbPage4Y],
-            [self.spbPage5X, self.spbPage5Y],
-            [self.spbPage6X, self.spbPage6Y]]
+        self.page_position=[[0, 0] for x in range(self.spbPagesPerSheet.value())]
+        for i in range(self.spbPagesPerSheet.value()):
+            self.comboPosition.addItem("Page {0}".format(i+1))
         self.density_render=150.0
         self.current_page=0
         #self.fileInput and self.fileOutput need to be defined first
@@ -61,6 +58,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         e-mail: cges30901@gmail.com<br>
         License: GPL v3'''))
 
+    @pyqtSlot(int)
+    def on_spbPagesPerSheet_valueChanged(self, num):
+        while num>len(self.page_position):
+            self.comboPosition.addItem("Page {0}".format(len(self.page_position)+1))
+            self.page_position.append([0, 0])
+        while num<len(self.page_position):
+            self.comboPosition.removeItem(len(self.page_position)-1)
+            self.page_position.pop()
+        self.needPaint=True
+        self.update()
+
     def loadPdf(self):
         self.labelSelectPoint.setText(self.tr("Loading..."))
         self.labelSelectPoint.repaint()
@@ -96,10 +104,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             page=pdfInput.getPage(i)
             for j in range(pagesPerSheet):
                 page_crop=copy.copy(page)
-                page_crop.mediaBox.lowerLeft=(self.spbPage[j][0].value()*factor,
-                    self.spbPage[j][1].value()*factor+height)
-                page_crop.mediaBox.upperRight=(self.spbPage[j][0].value()*factor+width,
-                    self.spbPage[j][1].value()*factor)
+                page_crop.mediaBox.lowerLeft=(self.page_position[j][0]*factor,
+                    self.page_position[j][1]*factor+height)
+                page_crop.mediaBox.upperRight=(self.page_position[j][0]*factor+width,
+                    self.page_position[j][1]*factor)
                 pdfOutput.addPage(page_crop)
         outputStream = open(self.fileOutput, "wb")
         pdfOutput.write(outputStream)
@@ -166,8 +174,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         sheetHeight=self.pdfPage.pageSizeF().height()*self.density_render/72
         pageHeight=self.spbHeight.value()
         for i in range(len(rows)*len(columns)):
-            self.spbPage[i][0].setValue(columns[i%len(columns)])
-            self.spbPage[i][1].setValue(sheetHeight-pageHeight-rows[i//len(columns)])
+            self.page_position[i][0]=columns[i%len(columns)]
+            self.page_position[i][1]=sheetHeight-pageHeight-rows[i//len(columns)]
+            #update value of spbPositionX and spbPositionY
+        self.spbPositionX.setValue(self.page_position[self.comboPosition.currentIndex()][0])
+        self.spbPositionY.setValue(self.page_position[self.comboPosition.currentIndex()][1])
         self.needPaint=True
         self.update()
         self.statusBar.showMessage(self.tr("Found {0} pages").format(len(rows)*len(columns)), 2000)
@@ -189,62 +200,55 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         #cannot find a page
         return QPoint(-1, -1)
 
+    @pyqtSlot(int)
+    def on_comboPosition_currentIndexChanged(self, num):
+        self.spbPositionX.setValue(self.page_position[num][0])
+        self.spbPositionY.setValue(self.page_position[num][1])
+
+    @pyqtSlot(int)
+    def on_spbPositionX_valueChanged(self, num):
+        self.page_position[self.comboPosition.currentIndex()][0]=num
+
+    @pyqtSlot(int)
+    def on_spbPositionY_valueChanged(self, num):
+        self.page_position[self.comboPosition.currentIndex()][1]=num
+
     @pyqtSlot(bool)
-    def on_btnPageOne_clicked(self):
+    def on_btnPosition_clicked(self):
         self.set=1
-        self.statusBar.showMessage(self.tr("Please click the upper left point of the first page"))
-
-    @pyqtSlot(bool)
-    def on_btnPageTwo_clicked(self):
-        self.set=2
-        self.statusBar.showMessage(self.tr("Please click the upper left point of the second page"))
-
-    @pyqtSlot(bool)
-    def on_btnPageThree_clicked(self):
-        self.set=3
-        self.statusBar.showMessage(self.tr("Please click the upper left point of the third page"))
-
-    @pyqtSlot(bool)
-    def on_btnPageFour_clicked(self):
-        self.set=4
-        self.statusBar.showMessage(self.tr("Please click the upper left point of the fourth page"))
-
-    @pyqtSlot(bool)
-    def on_btnPageFive_clicked(self):
-        self.set=5
-        self.statusBar.showMessage(self.tr("Please click the upper left point of the fifth page"))
-
-    @pyqtSlot(bool)
-    def on_btnPageSix_clicked(self):
-        self.set=6
-        self.statusBar.showMessage(self.tr("Please click the upper left point of the sixth page"))
+        self.statusBar.showMessage(self.tr(
+            "Please click the upper left point of page {0}".format(self.comboPosition.currentIndex()+1)))
 
     @pyqtSlot(bool)
     def on_btnWidthHeight_clicked(self):
-        self.set=7
+        self.set=2
         self.statusBar.showMessage(self.tr("Please click the upper left point of any page"))
 
     def labelSelectPoint_mousePressed(self, x, y):
-        if self.set>=1 and self.set <=6:
-            #store coordinate in spbPage[self.set-1]
-            self.spbPage[self.set-1][0].setValue(x)
+        if self.set==1:
+            #self.spbHeight.value() should be set first for y coordinate
+            if self.spbHeight.value()==0:
+                QMessageBox.warning(self, self.tr("Warning"), self.tr("Height should be set first"))
 
-            #point (0,0) is in lowerLeft, so coordinate need to be changed
+            #set x coordinate
+            self.spbPositionX.setValue(x)
+
+            #point (0,0) is in lowerLeft, so y coordinate need to be changed
             sheetHeight=self.pdfPage.pageSizeF().height()*self.density_render/72
             pageHeight=self.spbHeight.value()
-            self.spbPage[self.set-1][1].setValue(sheetHeight-pageHeight-y)
+            self.spbPositionY.setValue(sheetHeight-pageHeight-y)
             self.needPaint=True
             self.set=0
             self.statusBar.clearMessage()
-        elif self.set==7:
+        elif self.set==2:
             #set Width and Height - step one
             #store coordinate of upper left point
             self.upperleftX=x
             self.upperleftY=y
             #set lower right point if pressed again
-            self.set=8
+            self.set=3
             self.statusBar.showMessage(self.tr("Please click the lower right point of the page you clicked"))
-        elif self.set==8:
+        elif self.set==3:
             #set Width and Height - step two
             #get coordinate of lower right point (x,y)
             #and calculate width and height
@@ -265,16 +269,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             for i in range(self.spbPagesPerSheet.value()):
                 sheetHeight=self.pdfPage.pageSizeF().height()*self.density_render/72
                 pageHeight=self.spbHeight.value()
-                path[i].moveTo(self.spbPage[i][0].value(),
-                    sheetHeight-pageHeight-self.spbPage[i][1].value())
-                path[i].lineTo(self.spbPage[i][0].value()+self.spbWidth.value(),
-                    sheetHeight-pageHeight-self.spbPage[i][1].value())
-                path[i].lineTo(self.spbPage[i][0].value()+self.spbWidth.value(),
-                    sheetHeight-pageHeight-self.spbPage[i][1].value()+self.spbHeight.value())
-                path[i].lineTo(self.spbPage[i][0].value(),
-                    sheetHeight-pageHeight-self.spbPage[i][1].value()+self.spbHeight.value())
-                path[i].lineTo(self.spbPage[i][0].value(),
-                    sheetHeight-pageHeight-self.spbPage[i][1].value())
+                path[i].moveTo(self.page_position[i][0],
+                    sheetHeight-pageHeight-self.page_position[i][1])
+                path[i].lineTo(self.page_position[i][0]+self.spbWidth.value(),
+                    sheetHeight-pageHeight-self.page_position[i][1])
+                path[i].lineTo(self.page_position[i][0]+self.spbWidth.value(),
+                    sheetHeight-pageHeight-self.page_position[i][1]+self.spbHeight.value())
+                path[i].lineTo(self.page_position[i][0],
+                    sheetHeight-pageHeight-self.page_position[i][1]+self.spbHeight.value())
+                path[i].lineTo(self.page_position[i][0],
+                    sheetHeight-pageHeight-self.page_position[i][1])
                 painter.drawPath(path[i])
             painter.end()
             self.labelSelectPoint.setPixmap(pixmap_draw)
