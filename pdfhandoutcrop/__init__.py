537566f3ec5edb99ac5281715af24e55dea639bd
Hsiu-Ming Chang <cges30901@gmail.com>
2018-06-28 12:51:57 +0800

    load translation

--- pdfhandoutcrop/__init__.py
+++ pdfhandoutcrop/__init__.py
@@ -1,10 +1,17 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QTranslator, QLocale
 from pdfhandoutcrop.mainwindow import MainWindow
 
 def main():
     
     app = QApplication(sys.argv)
+
+    translator=QTranslator()
+    translator.load(QLocale(), "pdfhandoutcrop", "_",
+        os.path.dirname(os.path.abspath(__file__))+"/language")
+    app.installTranslator(translator)
 
     w = MainWindow()
     w.show()
