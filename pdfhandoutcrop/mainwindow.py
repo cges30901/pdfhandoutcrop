e8112ba81643355011624b695adf858aa26340da
Hsiu-Ming Chang <cges30901@gmail.com>
2018-09-06 21:35:36 +0800

    version 0.3.0

--- pdfhandoutcrop/mainwindow.py
+++ pdfhandoutcrop/mainwindow.py
@@ -52,7 +52,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot()
     def on_action_About_triggered(self):
-        version="0.2.1"
+        version="0.3.0"
         QMessageBox.about(self, self.tr("About"), self.tr(
 '''<h3>PdfHandoutCrop {0}</h3><br>
 Author: Hsiu-Ming Chang<br>
