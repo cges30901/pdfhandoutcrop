782e818a8a265225874a3fcd81147c1d5fd3a79b
Hsiu-Ming Chang <cges30901@gmail.com>
2021-03-07 01:34:06 +0800

    Fix location of language files in packaging-windows.bat

--- packaging-windows.bat
+++ packaging-windows.bat
@@ -1,6 +1,6 @@
 pyinstaller -y -w -n pdfhandoutcrop ^
 -i icons\pdfhandoutcrop.ico ^
---add-data pdfhandoutcrop\language\*;pdfhandoutcrop\language ^
+--add-data pdfhandoutcrop\language\*;language ^
 --add-data pdfhandoutcrop\pdfhandoutcrop.png;pdfhandoutcrop ^
 --add-data="LICENSE.txt;." ^
 pdfhandoutcrop\__init__.py
