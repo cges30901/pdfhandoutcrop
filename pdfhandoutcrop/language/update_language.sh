b05e74ad4e2457c2181c4c809c86f8b244a3e33a
Hsiu-Ming Chang <cges30901@gmail.com>
2024-11-10 15:09:26 +0800

    Remove pdfhandoutcrop.pro and add update_language.sh
    pylupdate6 does not support project file,
    so I use script to update ts files

---
+++ pdfhandoutcrop/language/update_language.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pylupdate6 --no-obsolete --ts pdfhandoutcrop.ts --ts pdfhandoutcrop_it.ts --ts pdfhandoutcrop_nl.ts --ts pdfhandoutcrop_ru.ts --ts pdfhandoutcrop_zh_TW.ts ../mainwindow.py ../setlayoutdialog.py ../mainwindow.ui ../setlayoutdialog.ui
--- pdfhandoutcrop/pdfhandoutcrop.pro
+++
@@ -1,9 +0,0 @@
-SOURCES += mainwindow.py \
-    setlayoutdialog.py
-
-FORMS += mainwindow.ui \
-    setlayoutdialog.ui
-
-TRANSLATIONS=language/pdfhandoutcrop.ts \
-    language/pdfhandoutcrop_ru.ts \
-    language/pdfhandoutcrop_zh_TW.ts
